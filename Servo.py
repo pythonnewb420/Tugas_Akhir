7092f970fccba6820d4de2542f75aa5d6c1a26cf
Anthony S <92085498+Anthony-Suryajaya@users.noreply.github.com>
2022-05-09 20:42:58 +0700

    Add files via upload

--- Servo.py
+++ Servo.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
-from time import sleep, time
-
+import time
 
 # Pins for servo Driver Inputs and setup
 # Raspberry Pi is told that pin index is the same as with the BOARD diagram not the BCM diagram
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(37, GPIO.OUT)  # GPIO pin 37 is set as output signal for the servo
 servo = GPIO.PWM(37, 50)
@@ -12,8 +12,9 @@ servo.start(0)
 
 #Speed Calibration for Brushed DC servo using PWM and ChangeDutyCycle
 cdc = 5  #Servo is set so that the rudder angle will be at MAX starboard angle
-servo.ChangeDutyCycle(5)
-time.sleep(5)  # servo is shut OFF after 5s usage
+servo.ChangeDutyCycle(cdc)
+print(cdc)
+time.sleep(1)  # servo is shut OFF after 5s usage
 
 #command to stop the servo
 servo.stop()
