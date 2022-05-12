f2d967207c2c2f07b23ce4f5d3bb4d81241a1296
Anthony S <92085498+Anthony-Suryajaya@users.noreply.github.com>
2022-05-12 15:30:23 +0700

    Add files via upload

--- TA_CAS_OD.py
+++ TA_CAS_OD.py
@@ -32,6 +32,8 @@ import serial
 ############################
 #LiDAR Setup
 ser = serial.Serial("/dev/serial0", 115200,timeout=0) # mini UART serial device
+if ser.isOpen() == False:
+    ser.open() # open serial port if not open
 # - - - - - - - - - - - - - - - - - - - - - - - -
 ############################
 # read ToF data from TF-Luna
@@ -39,26 +41,26 @@ ser = serial.Serial("/dev/serial0", 115200,timeout=0) # mini UART serial device
 #
 #funct to read lidar data
 def read_tfluna_data():
-    while True:
-        counter = ser.in_waiting # count the number of bytes of the serial port
-        if counter > 8:
-            bytes_serial = ser.read(9) # read 9 bytes
-            ser.reset_input_buffer() # reset buffer
+        while True:
+            counter = ser.in_waiting # count the number of bytes of the serial port
+            if counter > 8:
+                bytes_serial = ser.read(9) # read 9 bytes
+                ser.reset_input_buffer() # reset buffer
 
-            if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # check first two bytes
-                distance = bytes_serial[2] + bytes_serial[3]*256 # distance in next two bytes
-                return distance
-
+                if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # check first two bytes
+                    distance = bytes_serial[2] + bytes_serial[3]*256 # distance in next two bytes
+                    return distance
 class lidar:
     def update(self):
         # Keep looping indefinitely until the thread is stopped
         while True:
-            # If the camera is stopped, stop the thread
             distance = read_tfluna_data()
-            return
+            return distance
+            
     def __init__(self):
         distance = Thread(target=self.update,args=())
         distance.start()
+        
 #
 ############################
 # Actuator
